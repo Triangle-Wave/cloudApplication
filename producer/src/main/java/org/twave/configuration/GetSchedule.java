60bdee268cca7e7d0d364a6f32a1f30d4914bb00
twaveLab <1340281297@qq.com>
2023-07-29 12:47:35 +0800

    优化了打印方法

--- producer/src/main/java/org/twave/configuration/GetSchedule.java
+++ producer/src/main/java/org/twave/configuration/GetSchedule.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 定时任务
+ * 定时任务，每500ms从Redis中获取一次当前进度，打印到控制台
  *
  * @author : twave
  * @date : 2023/7/28 17:28
@@ -28,19 +28,20 @@ public class GetSchedule {
      */
     @Scheduled(fixedRate = 500)
     public void getSchedule() {
+        // 用list接收Redis中的所有任务进度
         List<Map<String, String>> list = redisUtil.getAllSchedule();
+        // 如果list为空，说明当前没有任务在运行
         if (list == null || list.size() == 0) {
             System.out.print("\r当前没有任务在运行");
         } else {
+            // 打印信息
             System.out.print("\r所有任务进度：");
             for (Map<String, String> map : list) {
-                float progress = Float.parseFloat(list.get(0).get("now")) /
-                        Float.parseFloat(list.get(0).get("all")) * 100;
-                progress = Math.round(progress * 100) / 100f;
-                System.out.printf("任务%s 运行在%s上 进度%s; ",
+                System.out.printf("任务%s 运行在%s上 进度%s/%s; ",
                         map.get("taskId"),
                         map.get("serviceName"),
-                        progress + "%");
+                        map.get("now"),
+                        map.get("all"));
             }
         }
     }
